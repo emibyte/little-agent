e6477a5e01f07d2f8f14c9998d6bcc8beb747312
emibyte <fevymarine@gmail.com>
2025-09-15 22:36:18 +0200

    exception handling in listing files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,9 +7,12 @@ def get_files_info(working_directory, directory="."):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(path):
         return f'Error: "{directory}" is not a directory'
-    if os.path.isdir(path):
-        for filename in os.listdir(path):
-            filepath = os.path.join(path, filename)
-            report.append(f"  -{filename}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}")
-        return "\n".join(report)
+    try:
+        if os.path.isdir(path):
+            for filename in os.listdir(path):
+                filepath = os.path.join(path, filename)
+                report.append(f"  -{filename}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}")
+            return "\n".join(report)
+    except Exception as e:
+        return f"Error while listing files: {e}"
                 
