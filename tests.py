bbc880a2da955d053c391194516f16e13f5d0fa2
emibyte <fevymarine@gmail.com>
2025-09-16 20:12:53 +0200

    tests for getting file content

--- tests.py
+++ tests.py
@@ -1,13 +1,21 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Result for current directory")
-print(get_files_info("calculator", "."))
+# print("Result for current directory")
+# print(get_files_info("calculator", "."))
 
-print("Result for 'pkg' directory")
-print(get_files_info("calculator", "pkg"))
+# print("Result for 'pkg' directory")
+# print(get_files_info("calculator", "pkg"))
 
-print("Result for '/bin' directory")
-print(get_files_info("calculator", "/bin"))
+# print("Result for '/bin' directory")
+# print(get_files_info("calculator", "/bin"))
 
-print("Result for '../' directory")
-print(get_files_info("calculator", "../"))
+# print("Result for '../' directory")
+# print(get_files_info("calculator", "../"))
+
+# ---------------------------------------------
+
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat")) # this should return an error string
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
