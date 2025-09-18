dbb93cbf9bf0fbe5af8634d69dbab9f2b5b8847c
emibyte <fevymarine@gmail.com>
2025-09-18 21:13:51 +0200

    schemas with description of the functions that the llm can use

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,21 @@
 from functions.config import LIMIT_TO_TRUNCATE_FILE
+from google.genai import types
 import os
+
+schema_get_file_content = types.FunctionDeclaration(
+    name = "get_file_content",
+    description = "Get the contents of a specified file within the working directory.",
+    parameters = types.Schema(
+        type = types.Type.OBJECT,
+        properties = {
+            "file_path": types.Schema(
+                type = types.Type.STRING,
+                description = "The path of the file to get the content of, relative to the working directory.",
+            ),
+        },
+        required = ["file_path"],
+    )
+)
 
 def get_file_content(working_directory, file_path):
     file_contents = ""
--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,29 @@
 import os
 from subprocess import run, CalledProcessError
+from google.genai import types
+
+schema_run_python_file = types.FunctionDeclaration(
+    name = "run_python_file",
+    description = "Run a python file via the python interpreter installed on the system and returns the output of the interpreter",
+    parameters = types.Schema(
+        type = types.Type.OBJECT,
+        properties = {
+            "file_path": types.Schema(
+                type = types.Type.STRING,
+                description = "The path of the file to run via the python interpreter, relative to the working directory. Has to be a file with the file-extension .py",
+            ),
+            "args": types.Schema(
+                type = types.Type.ARRAY,
+                items = types.Schema(
+                    type = types.Type.STRING,
+                    description = "Optional arguments to pass to the Python file.",
+                ),
+                description = "Optional arguments to pass to the Python file.",
+            ),
+        },
+        required = ["file_path"],
+    )
+)
 
 def run_python_file(working_directory, file_path, args=[]):
     path = os.path.join(working_directory, file_path)
